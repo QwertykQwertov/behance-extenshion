51818809b8ae5855d835ffad82dbb1a06c48c474
QwertykQwertov <qwertykqwertov@mail.ru>
2022-12-19 15:32:39 +0300

    first work version

--- app.js
+++ app.js
@@ -1,44 +1,57 @@
 console.log('Extend is ready!')
 
-let url = window.location.href
-urlIsChange(url)
-
 let imgArr = []
 const pattern = /\bhttps:\/\/mir-s3-cdn-cf.behance.net\/project_modules\//
 
-getImgs()
-
-console.log(window.location.href)
-
-function getImgs(){
-  imgArr = document.querySelectorAll('img')
-  window.setTimeout(showArray, 2000)
+window.onload = () => {
+  this.getImgs()
 }
 
+function getImgs () {
+  imgArr = document.querySelectorAll('img')
+  this.showArray(imgArr)
+}
 
-function showArray () {
+function showArray (arr) {
   imgArr = Array.from(imgArr).filter((el) => {
     return pattern.test(el.currentSrc)
   })
+  // console.log(imgArr, "ARR")
   for (let i = 0; i < imgArr.length; i++) {
-    let srcset = imgArr[i].srcset.split(',')
-    console.log(srcset)
+    // Если корневой элемент body, скачиваем картинку и завершаем скрипт
+    if (imgArr[i].parentElement.nodeName === "BODY") {
+      saveImage(imgArr[i].currentSrc)
+      clearTimeout(refreshFunction);
+      return
+    }
+    const srcset = imgArr[i].srcset.split(',').filter(el => el != '')
+    let src = srcset[srcset.length - 1]
+    src = src.match(/https:\/\/\S+/)
+    console.log(src)
     let btnSave = document.createElement('a')
-    btnSave.setAttribute('href', imgArr[i].currentSrc)
+    btnSave.setAttribute('href', src[0])
     btnSave.addEventListener('click', function () { saveImage(imgArr[i].currentSrc) })
-    btnSave.style['background-color'] = '#ff0000bf'
-    btnSave.classList.add('Btn-button-CqT', 'Btn-inverted-GDL', 'Btn-normal-If5', 'Btn-shouldBlur-ZHs', 'Actions-moduleAction-pY1', 'Actions-moduleActionLink-ur1')
+    btnSave.style['background-color'] = '#ff0000'
+    btnSave.style.opacity = '0.75'
+    btnSave.onmouseover = function () {
+      btnSave.style.opacity = "1";
+    };
+    btnSave.onmouseleave = function () {
+      btnSave.style.opacity = "0.75";
+    }
+    btnSave.classList.add('link-selelctor', 'Btn-button-CqT', 'Btn-inverted-GDL', 'Btn-normal-If5', 'Btn-shouldBlur-ZHs', 'Actions-moduleAction-pY1', 'Actions-moduleActionLink-ur1')
     btnSave.innerHTML = `<div class="Btn-labelWrapper-_Re">
       <div class="Btn-label-QJi e2e-Btn-label">Скачать</div>
     </div>`
-    if (imgArr[i].parentElement.nodeName === "BODY") {
-      saveImage(imgArr[i].currentSrc)
-      return
-    }
-    if (imgArr[i].parentElement.classList.contains('ImageElement-root-kir')) {
-      imgArr[i].parentElement.parentElement.children[2].children[0].children[0].append(btnSave)
+    if (imgArr[i].parentElement.classList.contains('ImageElement-root-kir') && !imgArr[i].parentElement.parentElement.children[2]?.children[0]?.children[0]?.lastChild?.classList?.contains('link-selelctor')) {
+      imgArr[i].parentElement.parentElement?.children[2]?.children[0]?.children[0]?.append(btnSave)
+    } else if (imgArr[i].classList?.contains('grid__item-image') && !imgArr[i].parentElement.children[imgArr[i].parentElement.children.length - 1]?.lastChild?.classList?.contains('link-selelctor')) {
+      imgArr[i].parentElement.children[imgArr[i].parentElement.children.length - 1].append(btnSave)
     } else {
-      imgArr[i].parentElement.children[2].children[0].children[0].append(btnSave)
+      if (!imgArr[i].parentElement.children[2]?.children[0]?.children[0]?.lastChild?.classList?.contains('link-selelctor')) {
+        imgArr[i].parentElement.children[2]?.children[0]?.children[0]?.append(btnSave)
+      }
+
     }
   }
 
@@ -50,17 +63,4 @@ function saveImage (url) {
   link.click()
 }
 
-
-
-function urlIsChange(currentUrl){
-  this.url = window.location.href
-  console.log('url is watching')
-  if (currentUrl == this.url) {
-    console.log('no change')
-    setTimeout(this.urlIsChange, 2000, this.url)
-  } else {
-    console.log('changed')
-    this.getImgs()
-    setTimeout(this.urlIsChange, 2000, this.url)
-  }
-}
+let refreshFunction = window.setInterval(getImgs, 3000)
